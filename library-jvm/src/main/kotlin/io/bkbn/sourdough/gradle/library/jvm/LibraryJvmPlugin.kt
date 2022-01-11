4fba2d06145630dd1e14d13960072c3c86dc20b1
Ryan Brink <rbweb@pm.me>
2022-01-11 09:26:53 -0500

    fix: dokka partial now generates as expected

--- library-jvm/src/main/kotlin/io/bkbn/sourdough/gradle/library/jvm/LibraryJvmPlugin.kt
+++ library-jvm/src/main/kotlin/io/bkbn/sourdough/gradle/library/jvm/LibraryJvmPlugin.kt
@@ -88,18 +88,15 @@ class LibraryJvmPlugin : Plugin<Project> {
   private fun Project.configureDokka() {
     plugins.withType(DokkaPlugin::class.java) {
       beforeEvaluate {
-        it.buildscript.apply {
-          dependencies.apply {
-            this.addProvider("classpath", provider { "org.jetbrains.dokka:versioning-plugin:1.6.0" })
-          }
-        }
+        it.buildscript.dependencies.add("classpath", "org.jetbrains.dokka:versioning-plugin:1.6.0")
       }
       tasks.withType(DokkaTaskPartial::class.java) {
+        val version = version.toString()
         dependencies.apply {
           addProvider("dokkaPlugin", provider { "org.jetbrains.dokka:versioning-plugin:1.6.0" })
         }
         it.pluginConfiguration<VersioningPlugin, VersioningConfiguration> {
-          setVersion(rootProject.version)
+          setVersion(version)
         }
         it.dokkaSourceSets.apply {
           configureEach { gdssb ->
